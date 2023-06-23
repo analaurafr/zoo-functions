e7f22cc05d7d651d6d4675195c545787c0201f67
analaurafr <laurafernandes@outlook.com>
2023-06-23 18:11:46 -0300

    testando requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
 
-const getSpeciesByIds = (ids) => {
-  // seu código aqui
+const getSpeciesByIds = (...ids) => {
+  if (ids.length === 0) {
+    return [];
+  }
+
+  const { species } = data;
+  return species.filter((animal) => ids.includes(animal.id));
 };
 
 module.exports = getSpeciesByIds;
