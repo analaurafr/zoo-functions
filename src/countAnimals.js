1680a3f0d46c4ec12b770234eb15051a0b108bf9
analaurafr <laurafernandes@outlook.com>
2023-06-23 19:45:54 -0300

    requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,22 @@
 const data = require('../data/zoo_data');
 
-const countAnimals = (animal) => {
-  // seu código aqui
+const countAnimals = ({ species, sex } = {}) => {
+  const residentsBySpecies = data.species.reduce((result, { name, residents }) => {
+    const filteredResidents = residents.filter(({ sex: residentSex }) => {
+      if (sex) {
+        return residentSex === sex;
+      }
+      return true;
+    });
+
+    return { ...result, [name]: filteredResidents.length };
+  }, {});
+
+  if (species) {
+    return residentsBySpecies[species] || 0;
+  }
+
+  return residentsBySpecies;
 };
 
 module.exports = countAnimals;
