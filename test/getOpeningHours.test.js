14b3e9f43850bc2aafcc21a0c95f8d4335ac9bf3
analaurafr <laurafernandes@outlook.com>
2023-06-24 00:18:45 -0300

    adicionando testes

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -28,4 +28,55 @@ describe('getOpeningHours', () => {
     const result = getOpeningHours('Wednesday', '09:00-PM');
     expect(result).toBe('The zoo is closed');
   });
+
+  it('deve retornar a string "The zoo is closed" quando o dia for Monday e o horário for 09:00-AM', () => {
+    const result = getOpeningHours('Monday', '09:00-AM');
+    expect(result).toBe('The zoo is closed');
+  });
+
+  it('deve retornar a string "The zoo is open" quando o dia for Tuesday e o horário for 09:00-AM', () => {
+    const result = getOpeningHours('Tuesday', '09:00-AM');
+    expect(result).toBe('The zoo is open');
+  });
+
+  it('deve retornar a string "The zoo is closed" quando o dia for Wednesday e o horário for 09:00-PM', () => {
+    const result = getOpeningHours('Wednesday', '09:00-PM');
+    expect(result).toBe('The zoo is closed');
+  });
+
+  it('deve lançar uma exceção com a mensagem "The day must be valid. Example: Monday" quando o dia for inválido', () => {
+    expect(() => {
+      getOpeningHours('Thu', '09:00-AM');
+    }).toThrow('The day must be valid. Example: Monday');
+  });
+
+  it('deve lançar uma exceção com a mensagem "The abbreviation must be \'AM\' or \'PM\'" quando a abreviação for inválida', () => {
+    expect(() => {
+      getOpeningHours('Friday', '09:00-ZM');
+    }).toThrow('The abbreviation must be \'AM\' or \'PM\'');
+  });
+
+  it('deve lançar uma exceção com a mensagem "The hour should represent a number" quando o formato da hora estiver incorreto', () => {
+    expect(() => {
+      getOpeningHours('Saturday', 'C9:00-AM');
+    }).toThrow('The hour should represent a number');
+  });
+
+  it('deve lançar uma exceção com a mensagem "The minutes should represent a number" quando o formato dos minutos estiver incorreto', () => {
+    expect(() => {
+      getOpeningHours('Sunday', '09:c0-AM');
+    }).toThrow('The minutes should represent a number');
+  });
+
+  it('deve lançar uma exceção com a mensagem "The hour must be between 0 and 12" quando a hora estiver fora do intervalo', () => {
+    expect(() => {
+      getOpeningHours('Monday', '13:00-AM');
+    }).toThrow('The hour must be between 0 and 12');
+  });
+
+  it('deve lançar uma exceção com a mensagem "The minutes must be between 0 and 59" quando os minutos estiverem fora do intervalo', () => {
+    expect(() => {
+      getOpeningHours('Tuesday', '09:60-AM');
+    }).toThrow('The minutes must be between 0 and 59');
+  });
 });
