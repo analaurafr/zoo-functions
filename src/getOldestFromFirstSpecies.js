8e7475105c9d5cb09c7cdb1fac97a914eb47d7f8
analaurafr <laurafernandes@outlook.com>
2023-06-24 15:51:11 -0300

    requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,22 @@
 const data = require('../data/zoo_data');
 
-const getOldestFromFirstSpecies = (id) => {
-  // seu código aqui
+const getEmployeeById = (id) => data.employees.find((employee) => employee.id === id);
+
+const getOldestFromFirstSpecies = (employeeId) => {
+  const employee = getEmployeeById(employeeId);
+
+  if (!employee || !employee.responsibleFor.length) return [];
+
+  const firstSpeciesId = employee.responsibleFor[0];
+  const species = data.species.find((specie) => specie.id === firstSpeciesId);
+
+  if (!species || !species.residents.length) return [];
+
+  const oldestAnimal = species.residents.reduce((oldest, animal) => (
+    animal.age > oldest.age ? animal : oldest
+  ));
+
+  return [oldestAnimal.name, oldestAnimal.sex, oldestAnimal.age];
 };
 
 module.exports = getOldestFromFirstSpecies;
