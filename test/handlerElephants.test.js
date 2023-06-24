bf84c2adfde5f26c8f0d5e670439ab88d00fc179
analaurafr <laurafernandes@outlook.com>
2023-06-24 00:33:35 -0300

    mais testes

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -31,3 +31,50 @@ describe('handlerElephants', () => {
     });
   });
 });
+
+describe('handlerElephants', () => {
+  it('deve retornar a quantidade de elefantes quando o argumento for "count"', () => {
+    const result = handlerElephants('count');
+    expect(result).toBe(4);
+  });
+
+  it('deve retornar um array com os nomes dos elefantes quando o argumento for "names"', () => {
+    const result = handlerElephants('names');
+    expect(result).toContain('Jefferson');
+  });
+
+  it('deve retornar a média de idade dos elefantes quando o argumento for "averageAge"', () => {
+    const result = handlerElephants('averageAge');
+    expect(result).toBeCloseTo(10.5);
+  });
+
+  it('deve retornar a localização dos elefantes dentro do Zoológico quando o argumento for "location"', () => {
+    const result = handlerElephants('location');
+    expect(result).toBe('Central Park');
+  });
+
+  it('deve retornar a popularidade dos elefantes quando o argumento for "popularity"', () => {
+    const result = handlerElephants('popularity');
+    expect(result).toBe('high');
+  });
+
+  it('deve retornar um array com os dias disponíveis para visitar os elefantes quando o argumento for "availability"', () => {
+    const result = handlerElephants('availability');
+    expect(result).toEqual(['segunda-feira', 'quarta-feira', 'sexta-feira']);
+  });
+
+  it('deve retornar undefined quando nenhum argumento for fornecido', () => {
+    const result = handlerElephants();
+    expect(result).toBe(undefined);
+  });
+
+  it('deve retornar uma mensagem de erro quando o argumento não for uma string', () => {
+    const result = handlerElephants(123);
+    expect(result).toBe('Parâmetro inválido, é necessário uma string');
+  });
+
+  it('deve retornar null quando o argumento não corresponder a nenhuma informação válida', () => {
+    const result = handlerElephants('invalid');
+    expect(result).toBe(null);
+  });
+});
