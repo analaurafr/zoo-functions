b3b4bcc5f943e224887dac15ba8c293901c9f3c6
analaurafr <laurafernandes@outlook.com>
2023-06-24 01:14:08 -0300

    revendo requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,29 @@
 const data = require('../data/zoo_data');
 
 const countEntrants = (entrants) => {
-  // seu código aqu
+  const childCount = entrants.filter((child) => child.age < 18).length;
+  const adultCount = entrants.filter((adult) => adult.age >= 18 && adult.age < 50).length;
+  const seniorCount = entrants.filter((senior) => senior.age >= 50).length;
+  const entrantsList = {
+    child: childCount,
+    adult: adultCount,
+    senior: seniorCount,
+  };
+  return entrantsList;
 };
 
 const calculateEntry = (entrants) => {
-  // seu código aqui
+  if (!entrants || entrants.length === 0) {
+    return 0;
+  }
+
+  const entrantsList = countEntrants(entrants);
+  const childCost = entrantsList.child * data.prices.child;
+  const adultCost = entrantsList.adult * data.prices.adult;
+  const seniorCost = entrantsList.senior * data.prices.senior;
+  const total = childCost + adultCost + seniorCost;
+
+  return parseFloat(total.toFixed(2));
 };
 
 module.exports = { calculateEntry, countEntrants };
